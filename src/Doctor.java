7866649e34ce6c41e258bf58881e35e7929a9541
Oscar Roman <oscarroman.pe@gmail.com>
2024-04-03 19:38:28 -0500

    Statics and methods I

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,10 +1,23 @@
 public class Doctor {
-    int id;
+    static int id = 0; // Autoincrement. Si no fuera static, la variable se reiniciaría, no sobreviviría
     String name;
     String speciality;
+
+    Doctor() {
+        System.out.println("Construyendo el Objeto Doctor");
+        id++;
+    }
+
+    Doctor (String name) {
+        System.out.println("El nombre del doctor asignado es: " + name);
+    }
 
     // Comportamientos
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import javax.print.Doc;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Anahi Salgado");
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        UIMenu.showMenu();
     }
+
+
 }
