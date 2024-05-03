607433033f6be16ae40787a03f0c3e1440020389
Oscar Roman <oscarroman.pe@gmail.com>
2024-05-02 21:28:47 -0500

    clases anidadas

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id = 0; // Autoincrement. Si no fuera static, la variable se reiniciaría, no sobreviviría.
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     Doctor() {
         System.out.println("Construyendo el Objeto Doctor");
@@ -22,5 +25,49 @@ public class Doctor {
 
     public void showId(){
         System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointment = new ArrayList<>();
+    public void addAvailableAppointment (Date date, String time) {
+        availableAppointment.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointment() {
+        return availableAppointment;
+    }
+
+    public static class AvailableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -10,23 +12,55 @@ public class Main {
         Hospital myHospital = new Hospital("María Auxiliadora");
         myHospital.nameHospital = "Arzobispo Loayza";
         myHospital.showNameHospital();
- */
+
         Insurance insurance1 = new Insurance("Rimac Seguros", "Perú");
         insurance1.showNameInsurance();
 
 
         // showMenu();
+        */
         Doctor myDoctor = new Doctor("Leslye Suárez", "Psicología");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+        System.out.println(myDoctor.getAvailableAppointment());
+
+        for (Doctor.AvailableAppointment aAppointment: myDoctor.getAvailableAppointment()) {
+            System.out.println(aAppointment.getDate() + " " + aAppointment.getTime());
+        }
+        
+        /*
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
-        System.out.println("\nPatient:");
+        int i = 0;
+        int b = 2;
+        b = i;
+        // b = 0
+        */
+
+        /*System.out.println("\nPatient:");
         Patient patient = new Patient("Alejandra", "hola@alejandra.com");
-        patient.setWeight(54.6);
+        Patient patient2 = new Patient("Anahi", "alejandra@gmail.com");
+
+        System.out.println(patient);
+        System.out.println(patient2);
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+        System.out.println("\n");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        /*patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("1234564888");
         System.out.println(patient.getPhoneNumber());
+         */
 
     }
-;}
+}
